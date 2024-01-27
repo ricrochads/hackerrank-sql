f9fa6fbb82f9d12a715bc9e11d543fc6195b2e4b
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-01-26 23:29:06 -0300

    Draw The Triangle 1

---
+++ MS SQL Server/draw_the_triangle_1.sql
@@ -0,0 +1,12 @@
+DECLARE @rowNumber INT = 20;
+
+WITH DrawingPattern AS (
+  SELECT @rowNumber AS RowNumber
+  UNION ALL
+  SELECT RowNumber - 1
+  FROM DrawingPattern
+  WHERE RowNumber > 0
+)
+SELECT REPLICATE('* ', RowNumber)
+FROM DrawingPattern
+ORDER BY RowNumber DESC;
